d17ec46176f0b926aea9adb652d1aa641406e289
86400 <516161832@qq.com>
2018-09-03 16:39:45 +0800

    2018年9月3日16:39:37

--- zx-bt-persistent/src/main/java/com/zx/bt/persistent/service/PersistentService.java
+++ zx-bt-persistent/src/main/java/com/zx/bt/persistent/service/PersistentService.java
@@ -31,85 +31,90 @@ public class PersistentService {
     private InfoHashIpRespository infoHashIpRespository;
 
     public void persistent(Object obj) {
-        try {
-            if (obj instanceof Node) {
-                persistentNode((Node) obj);
-            } else if (obj instanceof InfoHash) {
-                persistentInfoHash((InfoHash) obj);
-            } else {
-                log.warn("未知对象;{}，不能持久化内容:{}", obj.getClass(), obj);
-            }
-        } catch (Exception e) {
-            log.error("持久化对象失败:{}", obj, e);
+        if (obj instanceof Node) {
+            persistentNode((Node) obj);
+        } else if (obj instanceof InfoHash) {
+            persistentInfoHash((InfoHash) obj);
+        } else {
+            log.warn("未知对象;{}，不能持久化内容:{}", obj.getClass(), obj);
         }
     }
 
     @Transactional
     public void persistentNode(Node node) {
-        try {
-            NodeId nodeId = nodeIdRepository.findByNodeId(node.getNodeId());
-            if (nodeId == null) {
-                nodeId = new NodeId(node);
-            } else {
-                Date now = new Date();
-                nodeId.setContractCount(nodeId.getContractCount() + 1);
-                nodeId.setLastUpdateTime(now);
-                nodeId.setLastContractDate(now);
-            }
-            log.info("{}nodeId:{}", nodeId.getId() == null ? "新增" : "更新", nodeId);
+        NodeId nodeId = nodeIdRepository.findByNodeId(node.getNodeId());
+        if (nodeId == null) {
+            nodeId = new NodeId(node);
+        } else {
+            Date now = new Date();
+            nodeId.setContractCount(nodeId.getContractCount() + 1);
+            nodeId.setLastUpdateTime(now);
+            nodeId.setLastContractDate(now);
+        }
+//            log.info("{}nodeId:{}", nodeId.getId() == null ? "新增" : "更新", nodeId);
 
+        try {
             nodeIdRepository.save(nodeId);
-            NodeIp nodeIp = nodeIpRepository.findByNodeIdAndIpAndPort(nodeId.getId(), node.getIp(), node.getPort());
-            if (nodeIp == null) {
-                nodeIp = new NodeIp(node);
-            } else {
-                nodeIp.setContractCount(nodeIp.getContractCount() + 1);
-                nodeIp.setLastContractDate(new Date());
-                nodeIp.setLastUpdateTime(new Date());
-            }
-            nodeIp.setNodeId(nodeId.getId());
-            log.info("{}nodeIp:{}", nodeIp.getId() == null ? "新增" : "更新", nodeIp);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        NodeIp nodeIp = nodeIpRepository.findByNodeIdAndIpAndPort(nodeId.getId(), node.getIp(), node.getPort());
+        if (nodeIp == null) {
+            nodeIp = new NodeIp(node);
+        } else {
+            nodeIp.setContractCount(nodeIp.getContractCount() + 1);
+            nodeIp.setLastContractDate(new Date());
+            nodeIp.setLastUpdateTime(new Date());
+        }
+        nodeIp.setNodeId(nodeId.getId());
+//            log.info("{}nodeIp:{}", nodeIp.getId() == null ? "新增" : "更新", nodeIp);
+        try {
             nodeIpRepository.save(nodeIp);
         } catch (Exception e) {
-            log.error("持久化Node异常", e);
+            log.error("", e);
+            throw new RuntimeException(e);
         }
     }
 
     @Transactional
     public void persistentInfoHash(InfoHash infoHash) {
-        try {
-            if (StringUtils.isBlank(infoHash.getInfoHash())) {
-                log.warn("infoHash 对象infoHash不能为空:{}", infoHash);
-                return;
-            }
+        if (StringUtils.isBlank(infoHash.getInfoHash())) {
+            log.warn("infoHash 对象infoHash不能为空:{}", infoHash);
+            return;
+        }
 
-            InfoHashId infoHashId = infoHashIdRespository.findByInfoHash(infoHash.getInfoHash());
-            if (infoHashId == null) {
-                infoHashId = new InfoHashId(infoHash);
-            } else {
-                infoHashId.setContractCount(infoHashId.getContractCount() + 1);
-                infoHashId.setLastContractDate(new Date());
-                infoHashId.setLastUpdateTime(new Date());
-            }
-            log.info("{}infoHashId:{}", infoHashId.getId() == null ? "新增" : "更新", infoHashId);
+        InfoHashId infoHashId = infoHashIdRespository.findByInfoHash(infoHash.getInfoHash());
+        if (infoHashId == null) {
+            infoHashId = new InfoHashId(infoHash);
+        } else {
+            infoHashId.setContractCount(infoHashId.getContractCount() + 1);
+            infoHashId.setLastContractDate(new Date());
+            infoHashId.setLastUpdateTime(new Date());
+        }
+//            log.info("{}infoHashId:{}", infoHashId.getId() == null ? "新增" : "更新", infoHashId);
+        try {
             infoHashIdRespository.save(infoHashId);
-            String peerAddress = infoHash.getPeerAddress().replaceAll(";", "");
-            String[] peerAddressArr = peerAddress.split(":");
-            String ip = peerAddressArr[0];
-            Integer port = Integer.valueOf(peerAddressArr[1]);
-            InfoHashIp infoHashIp = infoHashIpRespository.findByInfoHashIdAndIpAndPort(infoHashId.getId(), ip, port);
-            if (infoHashIp == null) {
-                infoHashIp = new InfoHashIp(infoHash);
-            } else {
-                infoHashIp.setContractCount(infoHashIp.getContractCount() + 1);
-                infoHashIp.setLastContractDate(new Date());
-                infoHashIp.setLastUpdateTime(new Date());
-            }
-            infoHashIp.setInfoHashId(infoHashId.getId());
-            log.info("{}infoHashIp:{}", infoHashIp.getId() == null ? "新增" : "更新", infoHashIp);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        String peerAddress = infoHash.getPeerAddress().replaceAll(";", "");
+        String[] peerAddressArr = peerAddress.split(":");
+        String ip = peerAddressArr[0];
+        Integer port = Integer.valueOf(peerAddressArr[1]);
+        InfoHashIp infoHashIp = infoHashIpRespository.findByInfoHashIdAndIpAndPort(infoHashId.getId(), ip, port);
+        if (infoHashIp == null) {
+            infoHashIp = new InfoHashIp(infoHash);
+        } else {
+            infoHashIp.setContractCount(infoHashIp.getContractCount() + 1);
+            infoHashIp.setLastContractDate(new Date());
+            infoHashIp.setLastUpdateTime(new Date());
+        }
+        infoHashIp.setInfoHashId(infoHashId.getId());
+//            log.info("{}infoHashIp:{}", infoHashIp.getId() == null ? "新增" : "更新", infoHashIp);
+        try {
             infoHashIpRespository.save(infoHashIp);
         } catch (Exception e) {
-            log.error("持久话infoHash异常", e);
+            throw new RuntimeException(e);
         }
     }
 
--- zx-bt-persistent/src/main/java/com/zx/bt/persistent/task/QueuePersistentTask.java
+++ zx-bt-persistent/src/main/java/com/zx/bt/persistent/task/QueuePersistentTask.java
@@ -4,42 +4,74 @@ import com.zx.bt.hashspider.function.Pauseable;
 import com.zx.bt.persistent.service.PersistentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @Slf4j
-public class QueuePersistentTask implements Pauseable{
+public class QueuePersistentTask implements Pauseable {
     @Autowired
     private RedisTemplate redisTemplate;
     @Autowired
     private PersistentService persistentService;
+    private int i = 0;
+    private ListOperations listOperations;
+
     @PostConstruct
     public void persitentStart() {
-        ListOperations listOperations = redisTemplate.opsForList();
-        log.info("SYSTEM_BT_DHT_QUEUE size:{}",listOperations.size("SYSTEM_BT_DHT_QUEUE"));
-        int i=0;
-        while(true){
-            ++i;
-            Object obj = listOperations.rightPop("SYSTEM_BT_DHT_QUEUE");
-            if(obj==null){
-                try {
-                    Thread.sleep(10000L);
-                    log.info("sleep");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                continue;
-            }
-            log.info("处理第{}个",(i+1));
-            persistentService.persistent(obj);
-            if(i%1000==0){
-                log.info("SYSTEM_BT_DHT_QUEUE size:{}",listOperations.size("SYSTEM_BT_DHT_QUEUE"));
-            }
-        }
+        listOperations = redisTemplate.opsForList();
+        new Thread(() -> {
+            log.info("SYSTEM_BT_DHT_QUEUE size:{}", listOperations.size("SYSTEM_BT_DHT_QUEUE"));
+            LinkedBlockingQueue<Runnable> queue = new LinkedBlockingQueue<>(10000000);
+            ThreadPoolExecutor pool = new ThreadPoolExecutor(100, 500, 1, TimeUnit.DAYS, queue);
+            while (true) {
+                pool.execute(() -> {
+                    Object obj = null;
+                    if ((obj = listOperations.rightPop("SYSTEM_BT_DHT_QUEUE")) == null) {
+                        log.info("没有,sleep");
+                        try {
+                            Thread.sleep(5000L);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return;
+                    }
+                    addI();
+                    try {
+                        persistentService.persistent(obj);
+                    }catch (RuntimeException e){
+                        listOperations.leftPush("SYSTEM_BT_DHT_QUEUE", obj);
+                        log.info("DataIntegrityViolationException");
+                    } catch (Exception e) {
+                        listOperations.leftPush("SYSTEM_BT_DHT_QUEUE", obj);
 
+                        log.error("出现异常，睡眠", e);
+                        try {
+                            Thread.sleep(5000L);
+                        } catch (InterruptedException e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                });
+
+            }
+        }).start();
+    }
+
+    private synchronized void addI() {
+        ++i;
+        if (i % 100 == 0) {
+            log.info("SYSTEM_BT_DHT_QUEUE处理数量:{}", i);
+        }
+        if (i % 1000 == 0) {
+            log.info("SYSTEM_BT_DHT_QUEUE size:{}", listOperations.size("SYSTEM_BT_DHT_QUEUE"));
+        }
     }
 }
